6f8283019be7929ed0545ce9561ed8d129990879
adityafajarsy <adityafajar.sy90@gmail.com>
2025-11-08 12:18:37 +0700

    sedikit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -80,7 +80,7 @@ export default function Hero() {
     });
   });
 
-  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+  const getVideoSrc = (index) => `/videos/hero-${index}.mp4`;
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
